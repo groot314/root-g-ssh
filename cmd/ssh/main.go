788f32b3a05b0c079f31f5f23c8611172cb02688
Geoff Root <groot314@gmail.com>
2025-09-07 19:56:58 -0400

    logging for start and stop and error of ssh server

--- cmd/ssh/main.go
+++ cmd/ssh/main.go
@@ -10,7 +10,6 @@ import (
 	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/log"
 	"github.com/charmbracelet/ssh"
 	"github.com/charmbracelet/wish"
 	"github.com/charmbracelet/wish/activeterm"
@@ -35,25 +34,25 @@ func main() {
 		),
 	)
 	if err != nil {
-		log.Error("Could not start server", "error", err)
+		logger.Log().Error("Could not start server", "error", err)
 	}
 
 	done := make(chan os.Signal, 1)
 	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
-	log.Info("Starting SSH server", "host", host, "port", port)
+	logger.Log().Info("Starting SSH server", "host", host, "port", port)
 	go func() {
 		if err = s.ListenAndServe(); err != nil && !errors.Is(err, ssh.ErrServerClosed) {
-			log.Error("Could not start server", "error", err)
+			logger.Log().Error("Could not start server", "error", err)
 			done <- nil
 		}
 	}()
 
 	<-done
-	log.Info("Stopping SSH server")
+	logger.Log().Info("Stopping SSH server")
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer func() { cancel() }()
 	if err := s.Shutdown(ctx); err != nil && !errors.Is(err, ssh.ErrServerClosed) {
-		log.Error("Could not stop server", "error", err)
+		logger.Log().Info("Could not stop server", "error", err)
 	}
 }
 
